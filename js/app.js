5f67619618bfa0a17efd2d86c36fc37721523833
Sindury <2022150480572@tesjo.edu.mx>
2026-04-16 12:00:29 -0600

    Modificacion

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function ejemplo(){
+    const inp_nombre = document.getElementById("name")
+    const nombre = inp_nombre.value
+    alert("Se hizo click en el boton, nombre: "+ nombre);
+}
+
+function BuscarCliente (nombre){
+    console.log (nombre)
+}
